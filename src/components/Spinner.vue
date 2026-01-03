1757eaa922fafc9792fc581414868329b4b1eb1a
LuizFerK <luizfernandoklein@live.com>
2026-01-03 14:02:05 -0300

    feat: Loading state on main screen

---
+++ src/components/Spinner.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Loader2 } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="flex items-center justify-center w-full h-full p-4">
+    <Loader2 class="animate-spin text-gray-200" :size="48" />
+  </div>
+</template>
